3a9e87f2f6c1ca9430566f889baebf6aa5b18333
jsschlosser <jsschlosser1981@gmail.com>
2026-01-15 19:30:45 -0500

    removed extra files, updated dark cal plotting to include DarkRead, added new plotting routine and improved image visualization

--- Dark_cal_data_processing.py
+++ Dark_cal_data_processing.py
@@ -6,7 +6,7 @@ from pylab import rcParams
 import os
 import sys
 
-def run(pathto_raw_dark_cal_data=None):
+def DarkCurrent(pathto_raw_dark_cal_data=None):
 	"""
 	Takes LHMP dark measurements (in DN) and plots them against temparature. Requires numpy, matplotlib, and pylab.
 
@@ -14,13 +14,13 @@ def run(pathto_raw_dark_cal_data=None):
 	:type pathto_raw_dark_cal_data: string		
 
 	"""	
-	pathto_raw_dark_cal_data = '../DarkTest_data.nc'
+	pathto_raw_dark_cal_data = '../DarkCurrent.nc'
 	dark_cal_data = Dataset(pathto_raw_dark_cal_data,'r')
 	dark_cal_data_dictionary = {}
 	for key in dark_cal_data.variables.keys():
 		print(key)
 		vals = dark_cal_data.variables[key][:]
-		dark_cal_data_dictionary[key] = vals#np.where(vals == '--', np.nan, vals)
+		dark_cal_data_dictionary[key] = np.where(vals == '--', np.nan, vals)#vals#
 
 	fs =14
 	lw = 1.5	
@@ -32,7 +32,7 @@ def run(pathto_raw_dark_cal_data=None):
 	#dict_name = '../Image_Dictionary_1.npy' #input("Enter the file name for analysis: ")	
 	#data_file_dictionary = dict_reconfig(np.load(dict_name,allow_pickle='TRUE'))
 	image_data = dark_cal_data_dictionary['Raw_Signal']
-	print(image_data[image_data>0])
+	#print(image_data[image_data>0])
 	sensor_temp = dark_cal_data_dictionary['Detector_Temperature']
 	Img_Data = image_data.reshape(len(sensor_temp),-1)
 	#time_avgd_data = np.mean(image_data,axis=(1,2))
@@ -49,6 +49,46 @@ def run(pathto_raw_dark_cal_data=None):
 	ax2.set_ylabel(r"Sensor Signal (DN)")
 	ax2.set_ylim(0,np.nanmax(Img_Data,axis=(0,1)))
 	plt.tight_layout()
-	plt.savefig(f"DarkSignal_Temp_plt.jpg", dpi=300)
+	plt.savefig(f"../DarkCal/DarkCurrent_plt.jpg", dpi=300)
+	#plt.show() # function to display the plot
+	plt.close() # 
+
+def DarkRead():
+	pathto_raw_dark_cal_data = '../DarkRead.nc'
+	dark_cal_data = Dataset(pathto_raw_dark_cal_data,'r')
+	dark_cal_data_dictionary = {}
+	for key in dark_cal_data.variables.keys():
+		print(key)
+		vals = dark_cal_data.variables[key][:]
+		dark_cal_data_dictionary[key] = np.where(vals == '--', np.nan, vals)#vals#
+
+	fs =14
+	lw = 1.5	
+	plt.rcParams.update({'font.size': fs})
+	plt.rcParams['font.family'] = 'serif'
+	plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']#	
+
+	
+	#dict_name = '../Image_Dictionary_1.npy' #input("Enter the file name for analysis: ")	
+	#data_file_dictionary = dict_reconfig(np.load(dict_name,allow_pickle='TRUE'))
+	image_data = dark_cal_data_dictionary['Raw_Signal']
+	#print(image_data[image_data>0])
+	sensor_et = dark_cal_data_dictionary['Detector_Exposure_Time']
+	Img_Data = image_data.reshape(len(sensor_et),-1)
+	#time_avgd_data = np.mean(image_data,axis=(1,2))
+	#time_stdv_data = np.std(image_data,axis=(1,2))
+	rcParams['figure.figsize'] = 5, 3 # W, H
+	fig,ax2=plt.subplots(1) # create figure and subplot
+	#ax2.errorbar(sensor_temp,time_avgd_data, yerr=time_stdv_data, linestyle='none', elinewidth=1.5, ecolor='k', zorder=0, capsize=3.5)
+	#ax2.plot(sensor_temp,time_avgd_data,marker='o', color='r', linestyle='none', markeredgewidth=1.5, markersize=7.5, markeredgecolor='k', zorder=1)
+	for i in range(len(sensor_et)):
+		y = Img_Data[i,:]
+		x = np.squeeze(sensor_et[i]*np.ones((1,len(Img_Data[i,:]))))
+		ax2.scatter(x[y>0],y[y>0])
+	ax2.set_xlabel(r"Exposure Time ($\mu$s)")
+	ax2.set_ylabel(r"Sensor Signal (DN)")
+	ax2.set_ylim(0,np.nanmax(Img_Data,axis=(0,1)))
+	plt.tight_layout()
+	plt.savefig(f"../DarkCal/DarkRead_plt.jpg", dpi=300)
 	#plt.show() # function to display the plot
 	plt.close() # 
---
+++ TestPlot.py
@@ -0,0 +1,82 @@
+import os
+import numpy as np
+import polanalyser as pa
+from netCDF4 import Dataset
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+def DemosaicTest():
+	"""
+	Function for capturing samples with the LHMP.
+	:param pathto_raw_data_file: Path to raw image dataset.
+	:type pathto_raw_data_file: string	  
+	"""  
+	pathto_raw_data_file = '../BayerRG8_test.nc'
+	data = Dataset(pathto_raw_data_file,'r')
+	data_dictionary = {}		 
+	for key in data.variables.keys():
+		vals = data.variables[key][:]
+		data_dictionary[key] = np.where(vals == '--', np.nan, vals)
+	image_data = data_dictionary['Raw_Signal']
+	dataset_length = len(image_data[:,0,0])
+	h_pixel_length = len(image_data[0,:,0])
+	v_pixel_length = len(image_data[0,0,:])
+	angles = np.deg2rad([0, 45, 90, 135])
+	for i1 in range(0,dataset_length):
+		# Demosaic the raw image into four polarization channels (0, 45, 90, 135 degrees)
+		# The 'pa.COLOR_PolarRGB' option handles the combined RGGB-polarization filter array.
+		# The output is a set of 12 full-resolution images (R, G, B for each of the 4 angles).
+		img_000_brg, img_045_brg, img_090_brg, img_135_brg = pa.demosaicing(image_data[i1,:,:], pa.COLOR_PolarRGB) 	
+		# Calculate the Stokes vector per-pixel
+		image_list = [img_000_brg, img_045_brg, img_090_brg, img_135_brg]
+		img_stokes = pa.calcStokes(image_list, angles) 	
+
+		# Convert the Stokes vector to Intensity, DoLP and AoLP
+		img_intensity_bgr = pa.cvtStokesToIntensity(img_stokes)
+		img_dolp_bgr = pa.cvtStokesToDoLP(img_stokes)
+		img_aolp_bgr = pa.cvtStokesToAoLP(img_stokes)	
+
+		# 4. Visualization (optional)
+		# The results (s0, dolp, aolp) are full-resolution images.
+		# s0 represents the total intensity (a normal color image).
+
+		s0 = np.sum(img_intensity_bgr,axis=2)
+		norm_s0 = mcolors.Normalize(vmin=np.min(s0), vmax=np.max(s0))
+		s0_vis = (norm_s0(s0)*255).astype('uint8')
+		print(s0_vis)
+		total_intensity_png_name = f'../LeveL_1_data/S0_total_{i1}.tiff'
+		#cv2.imshow("Total Intensity (S0)", s0)
+		cv2.imwrite(total_intensity_png_name, s0_vis)
+		DOLP_png_name = f'../LeveL_1_data/DOLP_color_{i1}.tiff'
+		AOLP_png_name = f'../LeveL_1_data/AOLP_color_{i1}.tiff'
+		img_dolp_vis = (img_dolp_bgr * 255).astype('uint8')
+		img_aolp_vis =(img_aolp_bgr * 255).astype('uint8')
+				   
+		cv2.imwrite(DOLP_png_name, img_dolp_vis)
+		cv2.imwrite(AOLP_png_name, img_aolp_vis)
+		for i2 in [0,1,2]:
+			intensity_png_name = f'../LeveL_1_data/S{i2}_color_{i1}.tiff'
+			stks = img_stokes[:,:,:,i2]
+			norm_stokes = mcolors.Normalize(vmin=np.min(stks), vmax=np.max(stks))
+			img_stokes_vis = (norm_stokes(stks)*255).astype('uint8')
+			cv2.imwrite(intensity_png_name, img_stokes_vis)
+
+def standard_test():
+	pathto_raw_data_file = '../PolarizedDolp_BayerRG8_test.nc'
+	data = Dataset(pathto_raw_data_file,'r')
+	data_dictionary = {}		 
+	for key in data.variables.keys():
+		vals = data.variables[key][:]
+		data_dictionary[key] = np.where(vals == '--', np.nan, vals)
+	image_data = data_dictionary['Raw_Signal']
+	dataset_length = len(image_data[:,0,0])
+	h_pixel_length = len(image_data[0,:,0])
+	v_pixel_length = len(image_data[0,0,:])
+	angles = np.deg2rad([0, 45, 90, 135])
+	for i1 in range(0,dataset_length):
+		DOLP_png_name = f'../LeveL_1_data/DOLP_standard_{i1}.tiff'
+		dolp_mono = np.squeeze(image_data[i1,:,:])
+		img_dolp_vis = (dolp_mono * 255).astype('uint8')
+		#img_dolp_vis = pa.applyColorToDoP(dolp_mono) 
+		cv2.imwrite(DOLP_png_name, dolp_mono)
